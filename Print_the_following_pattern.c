c25d9a090c76378ae0470c161bbeb4b39cb88bf4
gayatrimamidipalli2003 <97426256+gayatrimamidipalli2003@users.noreply.github.com>
2022-05-25 22:17:29 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=1;i<n+1;i++)     {         for(j=1;j<n+1;j++)         {             if(i==j||i==n-j+1)             {                 printf("%d ",n-i+1);             }             else             {                 printf(" ",n-i+1);             }         }         printf(" ");     } }

--- Print_the_following_pattern.c
+++ Print_the_following_pattern.c
@@ -3,14 +3,20 @@ int main()
 {
     int i,j,n;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(i=1;i<n+1;i++)
     {
-        for(j=i;j<=n;j++)
+        for(j=1;j<n+1;j++)
         {
-            printf("%d ",j);
+            if(i==j||i==n-j+1)
+            {
+                printf("%d ",n-i+1);
+            }
+            else
+            {
+                printf(" ",n-i+1);
+            }
         }
         printf("
 ");
     }
-    return 0;
 }
