4c62655ffac1aeaba5e97e3673fb37c5b268bf14
gayatrimamidipalli2003 <97426256+gayatrimamidipalli2003@users.noreply.github.com>
2022-07-08 21:29:07 +0530

    #include<stdio.h> int main() {     int r,c,i,j,s1=0,s2=0;     scanf("%d%d",&r,&c);     int a[r][c];     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             if(i==0 || i==(r-1) || j==0 || j==(c-1))             {                 s1+=a[i][j];             }             else             {                 s2+=a[i][j];             }         }     }     printf("%d",s2); }

---
+++ Sum_of_non_boundary_values_in_a_matrix.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int r,c,i,j,s1=0,s2=0;
+    scanf("%d%d",&r,&c);
+    int a[r][c];
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            if(i==0 || i==(r-1) || j==0 || j==(c-1))
+            {
+                s1+=a[i][j];
+            }
+            else
+            {
+                s2+=a[i][j];
+            }
+        }
+    }
+    printf("%d",s2);
+}
