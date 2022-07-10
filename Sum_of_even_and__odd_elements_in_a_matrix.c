015f555b038cc7bdc638cccdb171371bb706a0c4
gayatrimamidipalli2003 <97426256+gayatrimamidipalli2003@users.noreply.github.com>
2022-07-10 15:51:26 +0530

    #include<stdio.h> int main() {     int r,c,i,j,s1=0,s2=0;     scanf("%d%d",&r,&c);     int a[r][c];     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             if(a[i][j]%2==0)             {                 s1+=a[i][j];             }             else             {                 s2+=a[i][j];             }         }     }     printf("%d %d",s1,s2); }

--- Sum_of_even_and__odd_elements_in_a_matrix.c
+++ Sum_of_even_and__odd_elements_in_a_matrix.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int r,c,i,j,se=0,so=0;
+    int r,c,i,j,s1=0,s2=0;
     scanf("%d%d",&r,&c);
     int a[r][c];
     for(i=0;i<r;i++)
@@ -17,13 +17,13 @@ int main()
         {
             if(a[i][j]%2==0)
             {
-                se+=a[i][j];
+                s1+=a[i][j];
             }
             else
             {
-                so+=a[i][j];
+                s2+=a[i][j];
             }
         }
     }
-    printf("%d %d",se,so);
+    printf("%d %d",s1,s2);
 }
