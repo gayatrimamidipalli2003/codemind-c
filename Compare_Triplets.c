4193bf818f7107af66f4a6d7f0dd4f325b4855ab
gayatrimamidipalli2003 <97426256+gayatrimamidipalli2003@users.noreply.github.com>
2022-09-06 19:09:49 +0530

    #include<stdio.h> int main() {     int a[3],b[3],x=0,y=0;     for(int i=0;i<3;i++)     {         scanf("%d",&a[i]);     }     for(int i=0;i<3;i++)     {         scanf("%d",&b[i]);     }     for(int i=0;i<3;i++)     {         if(a[i]>b[i])         {             x++;         }         else if(a[i]<b[i])         {             y++;         }     }     printf("%d %d",x,y); }

---
+++ Compare_Triplets.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a[3],b[3],x=0,y=0;
+    for(int i=0;i<3;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<3;i++)
+    {
+        scanf("%d",&b[i]);
+    }
+    for(int i=0;i<3;i++)
+    {
+        if(a[i]>b[i])
+        {
+            x++;
+        }
+        else if(a[i]<b[i])
+        {
+            y++;
+        }
+    }
+    printf("%d %d",x,y);
+}
