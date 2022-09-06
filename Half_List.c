828fb8e07b78f44b52da084e62deb961c68c9293
gayatrimamidipalli2003 <97426256+gayatrimamidipalli2003@users.noreply.github.com>
2022-09-06 18:24:53 +0530

    #include<stdio.h> int main() {     int m,n;     scanf("%d",&n);     int arr[n],a[n];     int i,j;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     m=n-1;     for(int i=0;i<n/2;i++)     {         a[i]=arr[m];         m--;         printf("%d ",a[i]);     }     m=0;     for(int i=n/2,j=0;i<n;i++,j++)     {         a[i]=arr[j];         m++;         printf("%d ",a[i]);     } }

---
+++ Half_List.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int m,n;
+    scanf("%d",&n);
+    int arr[n],a[n];
+    int i,j;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    m=n-1;
+    for(int i=0;i<n/2;i++)
+    {
+        a[i]=arr[m];
+        m--;
+        printf("%d ",a[i]);
+    }
+    m=0;
+    for(int i=n/2,j=0;i<n;i++,j++)
+    {
+        a[i]=arr[j];
+        m++;
+        printf("%d ",a[i]);
+    }
+}
