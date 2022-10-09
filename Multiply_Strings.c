fbc31269702d2731a3f6784495bea32eec2130e7
gayatrimamidipalli2003 <97426256+gayatrimamidipalli2003@users.noreply.github.com>
2022-10-10 04:16:46 +0530

    #include<stdio.h> #include<stdlib.h> int main() {     char a[10],b[10];     long long x,y;     scanf("%s %s",a,b);     x=atoi(a);     y=atoi(b);     printf("%lld",x*y); }

---
+++ Multiply_Strings.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    char a[10],b[10];
+    long long x,y;
+    scanf("%s
+%s",a,b);
+    x=atoi(a);
+    y=atoi(b);
+    printf("%lld",x*y);
+}
